81aecbc1a519af1bff2ca17ac942aef0a233fcc0
alfredohodes <alfredo.hodes@gmail.com>
2019-12-13 21:53:47 -0300

    Sobrecargados métodos plus, div y multiply para Dinero

--- src/main/groovy/aerorep/Dinero.groovy
+++ src/main/groovy/aerorep/Dinero.groovy
@@ -14,4 +14,20 @@ class Dinero {
         if (monto < 0) throw new IllegalArgumentException("El monto tiene que ser mayor o igual a 0")
         this.monto = monto
   }
+
+  def plus(Dinero otro) {
+      new Dinero(this.monto + otro.monto)
+  }
+
+  def div(Number divNbr) {
+      new Dinero(this.monto / divNbr)
+  }
+
+  def multiply(Number multNbr) {
+    new Dinero(this.monto * multNbr)
+  }
+
+  def multiply(Integer multInt) {
+    new Dinero(this.monto * multInt)
+  }
 }
